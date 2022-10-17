2bd8f869c82fb773bf43fcb565211e3b4d14fbb2
dart <dartfnm@yandex.ru>
2022-10-17 07:02:12 +0300

    1

---
+++ Operators/Types/user/fenomen/proj/cafe22/cafe22demo.cs
@@ -0,0 +1,12 @@
+using T3.Core.Operator;
+using T3.Core.Operator.Attributes;
+using T3.Core.Operator.Slots;
+
+namespace T3.Operators.Types.Id_57d5cd00_c8a6_44aa_8455_d964d4b26acd
+{
+    public class cafe22demo : Instance<cafe22demo>
+    {
+
+    }
+}
+
